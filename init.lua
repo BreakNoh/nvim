0a2df7f8e2884d81f847a181e192657342502311
Breno <brematosmene@gmail.com>
2025-08-21 15:58:57 -0300

    primeira commit

---
+++ init.lua
@@ -0,0 +1,36 @@
+--Setup Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim";
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+	});
+end
+vim.opt.rtp:prepend(lazypath);
+require("plugins");
+
+--Config Sintaxe
+vim.opt.filetype = 'on';
+vim.opt.syntax = 'on';
+vim.opt.termguicolors = true;
+
+no_celular = os.getenv("TERMUX_VERSION");
+identacao = no_celular and 2 or 4;
+
+vim.opt.tabstop = identacao;
+vim.opt.shiftwidth = identacao;
+vim.opt.softtabstop = identacao;
+
+vim.opt.number = true;
+
+--Tema
+vim.cmd([[colorscheme dracula]])
+
+--Identar ao Salvar
+vim.api.nvim_create_augroup("AutoIndentGroup", { clear = true });
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = "AutoIndentGroup",
+	pattern = "*",
+	callback = function()
+		vim.cmd("normal! gg=G");
+	end,
+});
---
+++ lua/plugins.lua
@@ -0,0 +1,42 @@
+return require("lazy").setup ({
+	{
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		branch = 'main',
+		build = ':TSUpdate',
+	},
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		config = true,
+		opts = {
+			ensure_installed = {"lua", "markdown", "toml", "rust", "css", "html", "javascript"};
+		}
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = true,
+		opts = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "│" },
+				topdelete = { text = "┐" },
+				changedelete = { text = "│" },
+			},
+		},
+	}
+	"dracula/vim",
+})
